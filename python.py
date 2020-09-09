212c33cad243ff3a320b532a6f266b28707f1858
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-09 12:08:46 +0200

    Hello you in python af

---
+++ python.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+print("Hello you!, ik ben Zoey.")
+sleep(2)
+userInput = input("Wie ben jij?\n")
+sleep(2)
+print("Hello " + userInput)
+input() # Anders gaat de console instantly weg IIRC
